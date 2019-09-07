9f8c7b682b69bb16d00989dc4138706dfc5fe820
otsubo <eeeelllleeee24@gmail.com>
2019-09-07 23:39:32 +0900

    バリデーションの設定

--- app/House.php
+++ app/House.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    //
+    //値(id)がnullでもエラーなく動作するように設定
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'title' => 'required',
+        'content' => 'required',
+        'image_url' => 'required'
+    );
 }
